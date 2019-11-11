5b461f6e91ec4a1b0f2773611b2ca269e9c1adb5
Surbhi Kohli <32058209+Surbhi-Kohli@users.noreply.github.com>
2019-11-11 15:54:50 +0530

    Update HalloweenSaleHackerRank.cpp

--- HalloweenSaleHackerRank.cpp
+++ HalloweenSaleHackerRank.cpp
@@ -5,7 +5,8 @@ Usually, all games are sold at the same price,p dollars. However, they are
 planning to have the seasonal Halloween Sale next month in which you can 
 buy games at a cheaper price. Specifically, the first game you buy during the sale will be sold at p dollars,
 but every subsequent game you buy will be sold at exactly d dollars less than the cost of the previous one
-you bought. This will continue until the cost becomes less than or equal to m dollars, after which every game you buy will cost m dollars each.
+you bought. This will continue until the cost becomes less than or equal to m dollars, after which every game 
+you buy will cost m dollars each.
 
 For example, if p=20,d=3  and m=6 , then the following are the costs of the first 11 games you buy, in order:
 20,17,14,11,8,6,6,6,6,6,6
