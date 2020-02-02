765fb5af1a16ab55d1ab69038e2d6cdf258bbdb3
Surbhi Kohli <32058209+Surbhi-Kohli@users.noreply.github.com>
2020-02-02 12:50:54 +0530

    Update Array2dMaxSumhourglass.cpp

--- Array2dMaxSumhourglass.cpp
+++ Array2dMaxSumhourglass.cpp
@@ -1,3 +1,33 @@
+
+/*
+Given a 2D matrix, the task is that we find maximum sum of a hour glass.
+An hour glass is made of 7 cells
+in following form.
+    A B C
+      D
+    E F G
+   Example:
+   Input : 0 3 0 0 0
+        0 1 0 0 0
+        1 1 1 0 0
+        0 0 2 4 4
+        0 0 0 2 4
+   Output : 11
+   Below is the hour glass with
+   maximum sum
+1 0 0
+  4
+0 2 4
+
+It is evident from definition of hour glass that number of rows and number of columns must be equal to 3. 
+If we count total number of hour glasses in a matrix, we can say that the count is equal to count of possible
+top left cells in hour glass. Number of top-left cells in a hour glass is equal to (R-2)*(C-2). 
+Therefore, in a matrix total number of hour glass is (R-2)*(C-2)
+*/
+
+
+#include<bits/stdc++.h>
+using namespace std;
 int hourglassSum(vector<vector<int>> mat) {
 int ss=mat.size();
 //return ss;
@@ -28,10 +58,16 @@ int ss=mat.size();
 int main(){ 
     int n; 
    cin>>n
-  
     // Create a vector containing n  vectors of size n
     
-    vector<vector<int> > vec( n , vector<int> (m));  
+    vector<vector<int> > vec( n , vector<int> (n));  
+   for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            cin>>vec[i][j];
+        }
+    }
     int ans=hourglassSum(vec);
     cout<<ans;
     return 0;
