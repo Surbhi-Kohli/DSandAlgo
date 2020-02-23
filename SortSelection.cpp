93b6d48c17c47a9595f5964a87290e2bc517ccb8
Surbhi Kohli <32058209+Surbhi-Kohli@users.noreply.github.com>
2020-02-24 00:09:39 +0530

    Update SortSelection.cpp

--- SortSelection.cpp
+++ SortSelection.cpp
@@ -1,3 +1,19 @@
+/*
+The selection sort algorithm sorts an array by repeatedly finding the minimum element
+(considering ascending order) from unsorted part and putting it at the beginning. The algorithm 
+maintains two subarrays in a given array.
+
+1) The subarray which is already sorted.
+2) Remaining subarray which is unsorted.
+
+In every iteration of selection sort, the minimum element (considering ascending order) from the unsorted
+subarray is picked and moved to the sorted subarray.
+Time Complexity: O(n^2) as there are two nested loops.
+
+Auxiliary Space: O(1)
+The good thing about selection sort is it never makes more than O(n) swaps and can be useful when memory write is a costly operation.
+*/
+
 #include <iostream>
 using namespace std;
 
