b9e27a7268f20e861f4c1bb26c848cc443b905ba
Surbhi Kohli <32058209+Surbhi-Kohli@users.noreply.github.com>
2020-03-07 17:31:29 +0530

    Create GraphGenericTypeAdjacencyList.cpp

---
+++ GraphGenericTypeAdjacencyList.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include<list>
+#include<map>
+
+using namespace std;
+template <typename T>
+class Graph{
+    
+    map<T,list<T>> adjList;
+    public:Graph(){}
+    
+  
+  void addEdge(T u,T v,bool bidir=true)
+  {
+      adjList[u].push_back(v);
+      if(bidir)
+      {
+          adjList[v].push_back(u);
+      }
+  }
+  void printAdjlist(){
+      for(auto i:adjList)//auto will automatically determine the data type
+      {
+          cout<<i.first<<" -> ";
+          for(auto entry:i.second)
+          {
+           cout<<entry<<",";   
+          }
+          cout<<endl;
+      }
+  }
+  
+};
+int main() {
+   Graph <string>g;
+   g.addEdge("Putin","Trump",false);
+   g.addEdge("Putin","Modi",false);
+   g.addEdge("Modi","Trump",true);
+   
+   g.addEdge("Modi","Yogi");
+   g.addEdge("Yogi","Prabhu",false);
+   g.addEdge("Prabhu","Modi",false);
+  
+   g.printAdjlist();
+	return 0;
+}
