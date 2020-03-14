0d070112713b9d4ac7ffd692416c70f550e07dc0
Surbhi Kohli <32058209+Surbhi-Kohli@users.noreply.github.com>
2020-03-14 19:05:47 +0530

    Update StringsSanketStringsPerfectCB.cpp

--- StringsSanketStringsPerfectCB.cpp
+++ StringsSanketStringsPerfectCB.cpp
@@ -1,8 +1,11 @@
 /*
-Sanket has a string consisting of only 'a' and 'b' as the characters. Sanket describes perfectness of a string as the maximum length substring of equal characters. Sanket is given a number k which denotes the maximum number of characters he can change. Find the maximum perfectness he can generate by changing no more than k characters.
+Sanket has a string consisting of only 'a' and 'b' as the characters. Sanket describes perfectness of a string as
+the maximum length substring of equal characters. Sanket is given a number k which denotes the maximum number of 
+characters he can change. Find the maximum perfectness he can generate by changing no more than k characters.
 
 Input Format
-The first line contains an integer denoting the value of K. The next line contains a string having only ‘a’ and ‘b’ as the characters.
+The first line contains an integer denoting the value of K. The next line contains a string 
+having only ‘a’ and ‘b’ as the characters.
 
 Constraints
 2 ≤ N ≤ 10^5
