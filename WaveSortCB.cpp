40b59bdefc4799bb76feed563c8d37ead435df85
Surbhi Kohli <32058209+Surbhi-Kohli@users.noreply.github.com>
2020-03-14 16:12:14 +0530

    Update WaveSortCB.cpp

--- WaveSortCB.cpp
+++ WaveSortCB.cpp
@@ -29,11 +29,11 @@ void waveSort(int arr[],int n)
 {
     for(int i=0;i<n;i+=2)
     {
-        if(i>0 && arr[i-1]>arr[i])
+        if(i>0 && arr[i-1]<arr[i])
         {
             swap(arr[i],arr[i-1]);
         }
-        if(i<n-1 && arr[i+1]>arr[i])
+        if(i<n-1 && arr[i+1]<arr[i])
         {
             swap(arr[i],arr[i+1]);
         }
