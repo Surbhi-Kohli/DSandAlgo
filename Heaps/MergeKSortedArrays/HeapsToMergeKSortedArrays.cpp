f9a1ba680eb42ae2055d790d6af120e7e86955bc
Surbhi Kohli <32058209+Surbhi-Kohli@users.noreply.github.com>
2020-04-24 19:06:11 +0530

    Update HeapsToMergeKSortedArrays.cpp

--- Heaps/MergeKSortedArrays/HeapsToMergeKSortedArrays.cpp
+++ Heaps/MergeKSortedArrays/HeapsToMergeKSortedArrays.cpp
@@ -11,6 +11,30 @@ into an array is trivial: if a node is stored at index k, then its left child is
 and its right child at index 2k + 2.
 
 
+Given K sorted arrays each with N elements merge them and output the sorted array
+
+Input Format
+First line contains 2 space separated integers K and N.
+K is number of arrays.
+N is number of elements in each array.
+Next K lines contain N space separated integers each where ith line denotes the element of ith sorted array.
+
+Constraints
+Elements of array <= |10^15|
+N <= 10^5
+K <= 10
+
+Output Format
+Single line consisting of space separated numbers
+
+Sample Input
+3 4
+1 3 5 7
+2 4 6 8
+0 9 10 11
+Sample Output
+0 1 2 3 4 5 6 7 8 9 10 11
+
 
 */
 // C++ program to merge k sorted arrays of size n each. 
@@ -47,15 +71,23 @@ vector<int> mergeKSortedArrays(vector<vector<int>> arr)
 
 
 // Driver program to test above functions 
-int main() 
-{
-	vector<vector<int>> arr = {{2, 6, 12, 15}, 
-					{1, 9, 13,15}, 
-					{20, 24, 30,32 }}; 
-vector<int> output=mergeKSortedArrays(arr);
-	
-	for(auto x:output)
-	cout<<x<<" ";
-
-	return 0; 
-} 
+int main() {
+	int n,k;
+	cin>>k>>n;
+	  vector<vector<int> > arr( k,vector<int> (n)); //creating a 2d vector based on input values of n and k
+	//It will be vector with k rows and n cols
+	  for(int i=0;i<k;i++)
+	  {
+		  for(int j=0;j<n;j++)
+		  {
+			  cin>>arr[i][j];
+		  }
+	  }
+	vector<int> ans=  mergeKSOrtedArrays(arr);
+	  for(int i=0;i<n*k;i++)
+	  {
+		cout<<ans[i]<<" ";
+	  }
+	  cout<<endl;
+	return 0;
+}
