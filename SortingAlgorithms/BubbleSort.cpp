9e3ab3623d6bf7300d0138bacc5e29b62bafdc4a
Surbhi Kohli <32058209+Surbhi-Kohli@users.noreply.github.com>
2020-04-30 21:59:15 +0530

    Update BubbleSort.cpp

--- SortingAlgorithms/BubbleSort.cpp
+++ SortingAlgorithms/BubbleSort.cpp
@@ -7,8 +7,11 @@ Boundary Cases: Bubble sort takes minimum time (Order of n) when elements are al
 Sorting In Place: Yes
 Stable: Yes
 Adaptive:Can be made adaptive (if array is sorted in asc order: we can signal with flag that no swap is done,in 1st pass itself,
-   So Bubble Sort can be made adaptive)
-Take as input N, the size of array. Take N more inputs and store that in an array. Write a function that bubble sorts the array. Print the elements of sorted array.
+         So Bubble Sort can be made adaptive)
+Usage of Kth Pass:In the Kth pass of bubble sort,we get the kth largest element of an array    
+   
+Take as input N, the size of array. Take N more inputs and store that in an array. Write a function that bubble sorts the array.
+Print the elements of sorted array.
 1.It reads a number N.
 2.Take Another N numbers as input and store them in an Array.
 3.Bubble sort the array and print the Array.
