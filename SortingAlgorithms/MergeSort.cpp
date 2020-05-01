f696c9144829d6be17901ee434a35060a14f6523
Surbhi Kohli <32058209+Surbhi-Kohli@users.noreply.github.com>
2020-05-01 15:51:09 +0530

    Create MergeSort.cpp

---
+++ SortingAlgorithms/MergeSort.cpp
@@ -0,0 +1,138 @@
+/*
+Merge Sort
+Like QuickSort, Merge Sort is a Divide and Conquer algorithm. It divides input array in two halves, calls 
+itself for the two halves and then merges the two sorted halves. The merge() function is used for merging two halves. 
+The merge(arr, l, m, r) is key process that assumes that arr[l..m] and arr[m+1..r] are sorted and merges the two 
+sorted sub-arrays into one. See following C implementation for details.
+
+MergeSort(arr[], l,  r)
+If r > l
+     1. Find the middle point to divide the array into two halves:  
+             middle m = (l+r)/2
+     2. Call mergeSort for first half:   
+             Call mergeSort(arr, l, m)
+     3. Call mergeSort for second half:
+             Call mergeSort(arr, m+1, r)
+     4. Merge the two halves sorted in step 2 and 3:
+             Call merge(arr, l, m, r)
+         
+ Auxiliary Space: O(n)
+ Time Complexity: O(nLogn) 
+Algorithmic Paradigm: Divide and Conquer
+
+Sorting In Place: No in a typical implementation
+
+Stable: Yes
+
+Applications of Merge Sort
+Merge Sort is useful for sorting linked lists in O(nLogn) time.In the case of linked lists, the case is different mainly due to the difference in memory allocation of arrays and linked lists. Unlike arrays, linked list nodes may not be adjacent in memory. Unlike an array, in the linked list, we can insert items in the middle in O(1) extra space and O(1) time. Therefore merge operation of merge sort can be implemented without extra space for linked lists.
+In arrays, we can do random access as elements are contiguous in memory. Let us say we have an integer (4-byte) array A and let the address of A[0] be x then to access A[i], we can directly access the memory at (x + i*4). Unlike arrays, we can not do random access in the linked list. Quick Sort requires a lot of this kind of access. In linked list to access i’th index, we have to travel each and every node from the head to i’th node as we don’t have a continuous block of memory. Therefore, the overhead increases for quicksort. Merge sort accesses data sequentially and the need of random access is low.
+
+Inversion Count Problem
+Used in External Sorting        
+             
+PROBLEM STATEMENT:
+Given an array A, of N elements. Sort the array using mergesort algorithm.
+
+Input Format
+A single integer, N, denoting the number of elements in array. Next line contains N integers, denoting the elements of array.
+
+Constraints
+1<=N<=2*10^5 |Ai|<=10^9
+
+Output Format
+Print in a single line, N spaced integers, denoting the elements of array A in sorted order.
+
+Sample Input
+5
+3 6 4 1 2
+Sample Output
+1 2 3 4 6
+*/
+#include<iostream>
+using namespace std;
+void merge(int arr[], int l, int m, int r) 
+{ 
+    int i, j, k; 
+    int n1 = m - l + 1; 
+    int n2 =  r - m; 
+  
+    /* create temp arrays */
+    int L[n1], R[n2]; 
+  
+    /* Copy data to temp arrays L[] and R[] */
+    for (i = 0; i < n1; i++) 
+        L[i] = arr[l + i]; 
+    for (j = 0; j < n2; j++) 
+        R[j] = arr[m + 1+ j]; 
+  
+    /* Merge the temp arrays back into arr[l..r]*/
+    i = 0; // Initial index of first subarray 
+    j = 0; // Initial index of second subarray 
+    k = l; // Initial index of merged subarray 
+    while (i < n1 && j < n2) 
+    { 
+        if (L[i] <= R[j]) 
+        { 
+            arr[k] = L[i]; 
+            i++; 
+        } 
+        else
+        { 
+            arr[k] = R[j]; 
+            j++; 
+        } 
+        k++; 
+    } 
+  
+    /* Copy the remaining elements of L[], if there 
+       are any */
+    while (i < n1) 
+    { 
+        arr[k] = L[i]; 
+        i++; 
+        k++; 
+    } 
+  
+    /* Copy the remaining elements of R[], if there 
+       are any */
+    while (j < n2) 
+    { 
+        arr[k] = R[j]; 
+        j++; 
+        k++; 
+    } 
+} 
+  
+void mergeSort(int arr[], int l, int r) 
+{ 
+    if (l < r) 
+    { 
+        // Same as (l+r)/2, but avoids overflow for 
+        // large l and h 
+        int m = l+(r-l)/2; 
+  
+        // Sort first and second halves 
+        mergeSort(arr, l, m); 
+        mergeSort(arr, m+1, r); 
+  
+        merge(arr, l, m, r); 
+    } 
+} 
+int main() {
+	long long n;
+	cin>>n;
+	int arr[n];
+	for(int i=0;i<n;i++)
+	{
+		cin>>arr[i];
+	}
+	mergeSort(arr,0,n-1);
+	for(int i=0;i<n;i++)
+	{
+		cout<<arr[i]<<" ";
+	}
+	return 0;
+}
+
+
