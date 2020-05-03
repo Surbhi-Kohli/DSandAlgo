8bc09e85f0cf59b33d57ac228131cf2ddf77b735
Surbhi Kohli <32058209+Surbhi-Kohli@users.noreply.github.com>
2020-05-03 16:54:22 +0530

    Update RandomizedQuickSort.cpp

--- SortingAlgorithms/QuickSort/RandomizedQuickSort.cpp
+++ SortingAlgorithms/QuickSort/RandomizedQuickSort.cpp
@@ -3,6 +3,17 @@
  array in place such that all elements to the left of the pivot element are smaller, while all elements
  to the right of the pivot are greater that the pivot. Then we recursively call the same procedure for 
  left and right subarrays.
+ 
+  QuickSort is a Divide and Conquer algorithm. It picks an element as pivot and partitions the given array around
+  the picked pivot. There are many different versions of quickSort that pick pivot in different ways.
+
+Always pick first element as pivot.
+Always pick last element as pivot (implemented below)
+Pick a random element as pivot.
+Pick median as pivot.
+The key process in quickSort is partition(). Target of partitions is, given an array and an element x of
+array as pivot, put x at its correct position in sorted array and put all smaller elements (smaller than x) before x, and put all 
+greater elements (greater than x) after x. All this should be done in linear time.
 
 Unlike merge sort we don’t need to merge the two sorted arrays. Thus Quicksort requires lesser auxiliary 
 space than Merge Sort, which is why it is often preferred to Merge Sort.Using a randomly generated pivot we 
