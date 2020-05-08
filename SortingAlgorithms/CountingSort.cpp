3b10cd5c36190855cf88f8904cf96563f215c55c
Surbhi Kohli <32058209+Surbhi-Kohli@users.noreply.github.com>
2020-05-08 17:54:24 +0530

    Update CountingSort.cpp

--- SortingAlgorithms/CountingSort.cpp
+++ SortingAlgorithms/CountingSort.cpp
@@ -1,47 +1,66 @@
-#include<iostream>
-#include <vector> 
-#include <algorithm> 
+/*
+You are given an array, A. Sort this array using counting sort algorithm.
+
+Input Format
+First line contains a single integer, n, denoting the size of the array. Next line contains n integers, denoting the elements of the array.
+
+Constraints
+1<=N<=10^6 -10^6<=Ai<=10^6
+
+Output Format
+Print the array in sorted order.
+
+Sample Input
+5
+1 3 2 6 5
+Sample Output
+1 2 3 5 6
+
+*/
+
+#include<bits/stdc++.h>
+#include<climits>
 using namespace std;
-void countSort(vector <long long>& arr) 
-{ 
-    long long max = *max_element(arr.begin(), arr.end()); 
-    long long min = *min_element(arr.begin(), arr.end()); 
-    long long range = max - min + 1; 
-      
-    vector<long long> count(range), output(arr.size()); 
-    for(long long i = 0; i < arr.size(); i++) 
-        count[arr[i]-min]++; 
-          
-    for(long long i = 1; i < count.size(); i++) 
-           count[i] += count[i-1]; 
+void countSort(int arr[],int n)
+{
+    int max=*max_element(arr,arr+n);
+    int min=*min_element(arr,arr+n);
+   // cout<<"min is "<<min<<endl;
+    //cout<<"max is "<<max<<endl;
+    int len=max-min+1;
+    int count[len]={0};
+    for(int i=0;i<n;i++)
+    {
+        count[arr[i]-min]++;
+    }
     
-    for(long long i = arr.size()-1; i >= 0; i--) 
-    {  
-         output[ count[arr[i]-min] -1 ] = arr[i];  
-              count[arr[i]-min]--;  
-    } 
-      
-    for(long long i=0; i < arr.size(); i++) 
-            arr[i] = output[i]; 
-} 
-  
-void printArray(vector <long long> & arr)  
-{  
-    for (long long i=0; i < arr.size(); i++)  
-        cout << arr[i] << " ";  
-   
-} 
+    //  for(int i=0;i<len;i++)
+    //  {
+    //      cout<<count[i]<<" ";
+    //  }
+    // cout<<endl;
+	int output[n]={0};
+	int k=0;
+    for(int i=0;i<len;i++)
+    {
+        while(count[i]!=0)
+        {  output[k++]=i+min;
+            //cout<<i+min<<" ";
+            count[i]--;
+        }
+    }
+	for(int i=0;i<n;i++)
+	cout<<output[i]<<" ";
+}
 int main() {
-    vector<long long> arr;
-    long long n;
-    cin>>n;
-    for(long long i=0;i<n;i++)
-  {  int ans;
-  cin>>ans;
-      arr.push_back(ans);
-  }
-  countSort (arr); 
-    printArray (arr);
+	int n;
+	cin>>n;
+
+ int arr[n];
+	for(int i=0;i<n;i++)
+	{
+		cin>>arr[i];
+	}
+	countSort(arr,n);
 	return 0;
 }
-
