28028dd94ee1173ad88d5ecd8fb33b33dfe78fdb
Surbhi Kohli <32058209+Surbhi-Kohli@users.noreply.github.com>
2020-06-14 11:55:41 +0530

    Update EditDistanceRecursive.cpp

--- Dynamic Programming/EditDistance/EditDistanceRecursive.cpp
+++ Dynamic Programming/EditDistance/EditDistanceRecursive.cpp
@@ -1,5 +1,6 @@
 /*
-Given two strings str1 and str2 and below operations that can performed on str1. Find minimum number of edits (operations) required to convert ‘str1’ into ‘str2’.
+Given two strings str1 and str2 and below operations that can performed on str1. Find minimum number of edits
+(operations) required to convert ‘str1’ into ‘str2’.
 
 Insert
 Remove
@@ -29,8 +30,10 @@ Let us traverse from right corner, there are two possibilities for every pair of
 
 m: Length of str1 (first string)
 n: Length of str2 (second string)
-If last characters of two strings are same, nothing much to do. Ignore last characters and get count for remaining strings. So we recur for lengths m-1 and n-1.
-Else (If last characters are not same), we consider all operations on ‘str1’, consider all three operations on last character of first string, recursively compute minimum cost for all three operations and take minimum of three values.
+If last characters of two strings are same, nothing much to do. Ignore last characters and get count for
+remaining strings. So we recur for lengths m-1 and n-1.
+Else (If last characters are not same), we consider all operations on ‘str1’, consider all three operations on
+last character of first string, recursively compute minimum cost for all three operations and take minimum of three values.
 Insert: Recur for m and n-1
 Remove: Recur for m-1 and n
 Replace: Recur for m-1 and n-1
