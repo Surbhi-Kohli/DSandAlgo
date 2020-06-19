89484c2f5c9ed6e86786f07a654dfb95d2618aa0
Surbhi Kohli <32058209+Surbhi-Kohli@users.noreply.github.com>
2020-06-19 16:56:55 +0530

    Update KingdomDefense.cpp

--- GreedyAlgorithm/KingdomDefense.cpp
+++ GreedyAlgorithm/KingdomDefense.cpp
@@ -1,4 +1,22 @@
 /*   
+
+Theodre implements a new strategy game 'Defense of a Kingdom'.On each level,a player defends the kingdom that is represented by
+a rectangular grid of cells.The player builds crossbow towers in some cells of the grid.The tower defends all the cells  in the
+same row and in same column.No two towers share a row or col.
+
+The penalty of the position is the number of cells in the largest undefended rectangle.Help Theodre write a program that
+calculates the penalty of the given position.
+
+Input:The first line of the input contains the number of test cases.Each test case consist of a line with 3 int numbers 
+,w-width of the grid,h-height of the grid,n-no. of crossbow towers.Each of the following n lines containes 2 integer 
+numbers xi and yi-the coordinates of the cell occupied by a tower 
+Constraints: 1<=w,h<=40000 , 0<=n<=min(w,h)
+1<=xi<=w, 1<=yi<=h
+
+Output
+For each test case,output a single integer number-the no of cells in the largest rectangle that is not defended by the towers
+
+
 for input:
 1
 15 8 3
