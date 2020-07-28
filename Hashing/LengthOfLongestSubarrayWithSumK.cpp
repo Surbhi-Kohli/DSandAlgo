e1fc30d56c4719ad751f3ba4ceabf8265adba32f
Surbhi Kohli <32058209+Surbhi-Kohli@users.noreply.github.com>
2020-07-28 22:16:57 +0530

    Update LengthOfLongestSubarrayWithSumK.cpp

--- Hashing/LengthOfLongestSubarrayWithSumK.cpp
+++ Hashing/LengthOfLongestSubarrayWithSumK.cpp
@@ -1,3 +1,41 @@
+/*
+Longest sub-array having sum k
+Given an array arr[] of size n containing integers. The problem is to find the length of the longest sub-array having sum equal to the given value k.
+
+Examples:
+
+Input : arr[] = { 10, 5, 2, 7, 1, 9 }, 
+            k = 15
+Output : 4
+The sub-array is {5, 2, 7, 1}.
+
+Input : arr[] = {-5, 8, -14, 2, 4, 12},
+            k = -5
+Output : 5
+Naive Approach: Consider the sum of all the sub-arrays and return the length of the longest sub-array having sum ‘k’. Time Complexity is of O(n^2).
+
+
+Efficient Approach: Following are the steps:
+* Initialize sum = 0 and maxLen = 0.
+* Create a hash table having (sum, index) tuples.
+* For i = 0 to n-1, perform the following steps:
+   1.Accumulate arr[i] to sum.
+   2.If sum == k, update maxLen = i+1.
+   3.Check whether sum is present in the hash table or not. If not present, then add it to the hash table as (sum, i) pair.
+   4.Check if (sum-k) is present in the hash table or not. If present, then obtain index of (sum-k) from the hash table as index.
+      Now check if maxLen < (i-index), then update maxLen = (i-index).
+* Return maxLen.
+
+Time Complexity: O(n).
+Auxiliary Space: O(n).
+
+Input         Output
+3              2
+1 1 1
+2
+*/
+
+
 #include <iostream>
 #include <unordered_map>
 using namespace std;
