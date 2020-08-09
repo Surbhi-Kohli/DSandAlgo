cb65bbbca12e71b858f3719058228e5b95590f94
Surbhi Kohli <32058209+Surbhi-Kohli@users.noreply.github.com>
2020-08-09 17:43:02 +0530

    Update mergeTwoSortedLinkedList.cpp

--- LinkedLists/mergeTwoSortedLinkedList.cpp
+++ LinkedLists/mergeTwoSortedLinkedList.cpp
@@ -1,3 +1,14 @@
+#include <iostream>
+using namespace std;
+class node{
+    public:
+    int data;
+    node* next;
+    node(int d){
+       data=d;
+       next=NULL;
+    }
+};
 node* merge(node *a,node *b)
 {
     if(a==NULL)
@@ -19,4 +30,38 @@ node* merge(node *a,node *b)
         
     }
     return c;
+}
+int main() {
+	int n,m;
+	cin>>n;
+	
+	node *head1;
+	int x;
+	cin>>x;
+	head1=new node(x);
+	node *temp=head1;
+	for(int i=1;i<n;i++)
+	{
+	    cin>>x;
+	    temp->next=new node(x);
+	    temp=temp->next;
+	}
+	cin>>m;
+	node *head2;
+	cin>>x;
+	head2=new node(x);
+	node* temp2=head2;
+	for(int i=1;i<m;i++)
+	{
+	    cin>>x;
+	    temp2->next=new node(x);
+	    temp2=temp2->next;
+	}
+	node *ans=merge(head1,head2);
+	while(ans!=NULL)
+	{
+	    cout<<ans->data<<" ";
+	    ans=ans->next;
+	}
+	return 0;
 }
