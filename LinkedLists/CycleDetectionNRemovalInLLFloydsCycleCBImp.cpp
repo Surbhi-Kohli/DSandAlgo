093361a96119641f90f6b0602e6b31932aa40521
Surbhi Kohli <32058209+Surbhi-Kohli@users.noreply.github.com>
2020-08-12 00:05:18 +0530

    Update CycleDetectionNRemovalInLLFloydsCycleCBImp.cpp

--- LinkedLists/CycleDetectionNRemovalInLLFloydsCycleCBImp.cpp
+++ LinkedLists/CycleDetectionNRemovalInLLFloydsCycleCBImp.cpp
@@ -2,7 +2,8 @@
 We are given a linked list which contains a cycle. Detect that cycle and break it. Print the linked list after removing the cycle.
 
 Input Format
-The first line contains space separated integers. The integers are such that all the values are distinct but the value start repeating once the cycle gets completed. The list of integers given ends when -1 is input.
+The first line contains space separated integers. The integers are such that all the values are distinct but the value start
+repeating once the cycle gets completed. The list of integers given ends when -1 is input.
 
 Constraints
 n < 10^5 where n is the length of list without the cycle
@@ -16,7 +17,8 @@ Sample Output
 1 2 3 4 5
 
 Explanation
-Initially the first five elements are unique but starts repeating from 2. This means there is a link from 5 back to 2. So it represents a cycle. We have to break this cycle and print the list after breaking the cycle.
+Initially the first five elements are unique but starts repeating from 2. This means there is a link from 5 back to 2. So it 
+represents a cycle. We have to break this cycle and print the list after breaking the cycle.
 
 Sample Input //Important
 1 2 1 -1
@@ -43,7 +45,7 @@ class Node{
 };
 Node* cycleDetectionInLinkedList(Node *&head)
 {  //cout<<"in function "<<endl;
-    Node *slow=head;
+        Node *slow=head;
 	Node *fast=head;
     while(fast!=NULL && fast->next!=NULL)
    {
@@ -72,22 +74,21 @@ Node* cycleDetectionInLinkedList(Node *&head)
 int main() {
     int x;
 	Node * head=NULL;
-unordered_map<int,Node*> mp;
+        unordered_map<int,Node*> mp;
 	cin>>x;
 	//cout<<x<<"is value of x"<<endl;
 	head=new Node(x);
 	Node *temp=head;
-  mp[x]=head;
+       mp[x]=head;
 
 	cin>>x;
 	while(x!=-1)
 	{ 
-		if(mp.find(x)==mp.end())
+	   if(mp.find(x)==mp.end())
 	   {
-         temp->next=new Node(x);
+            temp->next=new Node(x);
 	    temp=temp->next;
 	      mp[x]=temp;
-	 
 	  }
 	  else{
 		  temp->next=mp[x];
