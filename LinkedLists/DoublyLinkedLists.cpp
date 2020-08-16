ae2185ae1299574a5f4373a46a2db1facd4e4296
Surbhi Kohli <32058209+Surbhi-Kohli@users.noreply.github.com>
2020-08-16 17:45:08 +0530

    Update DoublyLinkedLists.cpp

--- LinkedLists/DoublyLinkedLists.cpp
+++ LinkedLists/DoublyLinkedLists.cpp
@@ -1,3 +1,20 @@
+/*
+Input: 1 2 4 3 5 6 -1
+Output:
+DLL post inserting at head :
+6 5 3 4 2 1 
+**DLL on inserting a new Node before a Node**
+6 8 5 3 4 2 1 
+
+DLL on Inserting a Nodde after a node 
+6 8 5 99 3 4 2 1 
+
+DLL post Appending :
+6 8 5 99 3 4 2 1 100 
+
+
+*/
+
 #include <iostream>
 using namespace std;
 class Node{
@@ -30,13 +47,47 @@ void insertBeforeANode(Node **head,Node *beforeNode,int d)
 }
 void insertAfterNode(Node **head,Node *afterNode,int d)
 {
-    Node *newNode=new Node();
-    newNode->data=d;
-    Node* afterNext=afterNode->next;
+    if(afterNode==NULL)
+    {
+        cout<<"this cant be NULL";
+        return ;
+    }
+     Node *newNode=new Node();
+     newNode->data=d;
+    if((*head)==NULL)
+    { 
+      newNode->next=NULL;
+      newNode->prev=NULL;
+      (*head)=newNode;
+      return;
+    }
+   
+   
+      Node* afterNext=afterNode->next;
      afterNode->next=newNode;
       newNode->next=afterNext;
       afterNext->prev=newNode;
       
+}
+void append(Node **head,int d)
+{
+    Node *newNode=new Node();
+    newNode->data=d;
+    newNode->next=NULL;
+    if(*head==NULL)
+    {
+        newNode->prev=NULL;
+        (*head)=newNode;
+        return ;
+    }
+    Node* last=(*head);
+    while(last->next!=NULL)
+    {
+        last=last->next;
+    }
+    last->next=newNode;
+    newNode->next=NULL;
+    newNode->prev=last;
 }
 void print(Node* head)
 { 
@@ -46,7 +97,6 @@ void print(Node* head)
      head=head->next;
     }
 }
-
 int main() {
   //Creating DLL;
   Node *head=NULL;
@@ -57,14 +107,21 @@ int main() {
       insertAtHead(&head,x);
       cin>>x;
   }
+  cout<<"DLL post inserting at head :"<<endl;
    print(head);
   
    insertBeforeANode(&head, head->next, 8); 
-   cout<<endl<<"****************************";
+   cout<<endl<<"**DLL on inserting a new Node before a Node**";
    cout<<endl;
    print(head);
    cout<<endl;
+   
    insertAfterNode(&head,head->next->next,99);
+   cout<<endl<<"DLL on Inserting a Nodde after a node "<<endl;
+   print(head);
+   cout<<endl;
+   append(&head,100);
+   cout<<endl<<"DLL post Appending :"<<endl;
    print(head);
    return 0;
 }
