2e6ab9b62f9e84b9822b1900dcb7af1f12e06414
Surbhi Kohli <32058209+Surbhi-Kohli@users.noreply.github.com>
2020-08-24 02:26:08 +0530

    Create CheckForBalancedParanthesis.cpp

---
+++ Stacks/CheckForBalancedParanthesis.cpp
@@ -0,0 +1,70 @@
+/*
+Given an expression string exp, write a program to examine whether the pairs and the orders of “{“, “}”, “(“, “)”, “[“, “]” are correct in exp.
+
+Input:[()]{}{[()()]()}
+Output:The expression is balanced
+Input:[(])
+Output:The expression is not balanced
+*/
+
+#include <iostream>
+#include <stack>
+using namespace std;
+
+bool checkForBalancedParenthesis(string s)
+{
+    int n=s.length();
+    stack<char> st;
+    char ch;
+    for(int i=0;i<n;i++)
+    {
+      if(s[i]=='(' || s[i]=='{' || s[i]=='[')
+      {
+          st.push(s[i]);
+          continue;
+      }
+       if(st.empty())
+       {
+         return false;
+       }
+       switch(s[i])
+       {
+           case ')':
+            ch=st.top();
+           st.pop();
+           if(ch=='{'|| ch=='[')
+            return false;
+           break;
+           case '}':
+            ch=st.top();
+           st.pop();
+           if(ch=='(' || ch=='[')
+           return false;
+           break;
+           case ']':
+            ch=st.top();
+           st.pop();
+           if(ch=='(' || ch=='{')
+           return false;
+           break;
+           
+       }
+     
+    }
+    return st.empty();
+}
+
+int main() {
+
+string s;
+cin>>s;
+ bool ans=checkForBalancedParenthesis(s);
+  if(ans)
+  {
+      cout<<"The expression is balanced"<<endl;
+  }
+  else{
+      cout<<"The expression is not balanced"<<endl;
+  }
+	return 0;
+}
