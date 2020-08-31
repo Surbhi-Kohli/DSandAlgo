0472f4e7d6f73988e3ce8607c08e90d7bbc19e8a
Surbhi Kohli <32058209+Surbhi-Kohli@users.noreply.github.com>
2020-08-31 22:12:42 +0530

    Update EfficientMethod.cpp

--- F.A.A.N.G/RottenOranges/EfficientMethod.cpp
+++ F.A.A.N.G/RottenOranges/EfficientMethod.cpp
@@ -1,7 +1,9 @@
 /*
-Given a 2D array of dimension r*c where each cell in the array can have values 0, 1 or 2 which has the following meaning: 0 : Empty cell 1 : Cells have fresh oranges 2 : Cells have rotten oranges
+Given a 2D array of dimension r*c where each cell in the array can have values 0, 1 or 2 which has the following meaning: 0 : Empty cell 1 : Cells 
+have fresh oranges 2 : Cells have rotten oranges
 
-So, we have to determine what is the minimum time required to rot all oranges. A rotten orange at index [i,j] can rot other fresh orange at indexes [i-1,j], [i+1,j], [i,j-1], [i,j+1] (up, down, left and right) in unit time. If it is impossible to rot every orange then simply return -1.
+So, we have to determine what is the minimum time required to rot all oranges. A rotten orange at index [i,j] can rot other fresh orange at 
+indexes [i-1,j], [i+1,j], [i,j-1], [i,j+1] (up, down, left and right) in unit time. If it is impossible to rot every orange then simply return -1.
 
 Input Format
 The first line of input contains an integer T denoting the number of test cases.
