48bd27367ea397272c625e9467c50d4666bf0b66
Surbhi Kohli <32058209+Surbhi-Kohli@users.noreply.github.com>
2020-11-04 01:25:47 +0530

    Update and rename ArrayMaxSumCircularArray.cpp to Arrays/MaxSumCircularArray.cpp

--- Arrays/MaxSumCircularArray.cpp
+++ Arrays/MaxSumCircularArray.cpp
@@ -8,10 +8,14 @@ Sample Input                   output                     Explanation
   In this problem,there can be 2 cases ,ie ,either the subarray having max sum is obtained in a circular fashion or or subarray havinng 
   max sum is obtained in a non-circular fashion.
   
-  The non-circular maximum sum subarray can be obtained directly by Kadane's Algorithm.But the subarray obtained in circular way cannot be simply obtained.
+  The non-circular maximum sum subarray can be obtained directly by Kadane's Algorithm.But the subarray obtained in 
+  circular way cannot be simply obtained.
   
-  So to solve this problem,we will first calculate the max sum subarray using Kadane Algo,n store it in a variable, say ans1.Then we will invert the sign of every element of the array & again apply the Kadane Algo to calc max sum subarray of the inverted array.
-  But we should keep in mind the fact that the max sum of inverted subarray is actually the min sum subarray for the original array.so if we subtract the -ve value of this new sum from the cumulative sum of the original array,we will get another candidate for the answer n name it ans2.Then compare bothe and print the larger one.
+  So to solve this problem,we will first calculate the max sum subarray using Kadane Algo,n store it in a variable, say ans1.
+  Then we will invert the sign of every element of the array & again apply the Kadane Algo to calc max sum subarray of the inverted array.
+  But we should keep in mind the fact that the max sum of inverted subarray is actually the min sum subarray for the original array.
+  so if we subtract the -ve value of this new sum from the cumulative sum of the original array,we will get another candidate 
+  for the answer n name it ans2.Then compare bothe and print the larger one.
  
 */
 #include <bits/stdc++.h>
