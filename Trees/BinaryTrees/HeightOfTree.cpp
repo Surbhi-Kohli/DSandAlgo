d694fc03c130777558d723961853f753f03be89f
Surbhi Kohli <32058209+Surbhi-Kohli@users.noreply.github.com>
2021-01-23 15:57:34 +0530

    Update HeightOfTree.cpp

--- Trees/BinaryTrees/HeightOfTree.cpp
+++ Trees/BinaryTrees/HeightOfTree.cpp
@@ -1,4 +1,24 @@
 /*
+Write a Program to Find the Maximum Depth or Height of a Tree
+
+Algorithm:
+
+ maxDepth()
+1. If tree is empty then return 0
+2. Else
+     (a) Get the max depth of left subtree recursively  i.e., 
+          call maxDepth( tree->left-subtree)
+     (a) Get the max depth of right subtree recursively  i.e., 
+          call maxDepth( tree->right-subtree)
+     (c) Get the max of max depths of left and right 
+          subtrees and add 1 to it for the current node.
+         max_depth = max(max dept of left subtree,  
+                             max depth of right subtree) 
+                             + 1
+     (d) Return max_depth
+     
+Time Complexity of the above algo:O(n)  
+
 Input:3 4 -1 6 -1 -1 5 1 -1 -1 -1
 Output:3
 */
