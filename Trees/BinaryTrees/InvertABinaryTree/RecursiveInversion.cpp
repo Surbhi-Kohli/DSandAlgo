14bf1124f5ab7f9e1a6b214a661eb6f3908a559e
Surbhi Kohli <32058209+Surbhi-Kohli@users.noreply.github.com>
2021-01-30 01:03:23 +0530

    Update RecursiveInversion.cpp

--- Trees/BinaryTrees/InvertABinaryTree/RecursiveInversion.cpp
+++ Trees/BinaryTrees/InvertABinaryTree/RecursiveInversion.cpp
@@ -1,4 +1,11 @@
- 
+ /*
+ Since each node in the tree is visited only once, the time complexity is O(n),
+ where n is the number of nodes in the tree. We cannot do better than that, since at the very least 
+ we have to visit each node to invert it.
+
+Because of recursion, O(h) function calls will be placed on the stack in the worst case, where h is the height of the tree. 
+Because h\in O(n)h∈O(n), the space complexity is O(n).
+ */
  class Node{
  public:
   Node* left;
