4517ae2442a5c24cfd09982f202299c83e5d4944
Surbhi Kohli <32058209+Surbhi-Kohli@users.noreply.github.com>
2024-02-22 15:03:11 +0530

    Update MergeSort.cpp

--- SortingAlgorithms/MergeSort.cpp
+++ SortingAlgorithms/MergeSort.cpp
@@ -1,7 +1,8 @@
 /*
 Merge Sort
-Like QuickSort, Merge Sort is a Divide and Conquer algorithm. It divides input array in two halves, calls 
-itself for the two halves and then merges the two sorted halves. The merge() function is used for merging two halves. 
+Like QuickSort, Merge Sort is a Divide and Conquer algorithm. It divides input array in two
+halves, calls itself for the two halves and then merges the two sorted halves. 
+The merge() function is used for merging two halves. 
 The merge(arr, l, m, r) is key process that assumes that arr[l..m] and arr[m+1..r] are sorted and merges the two 
 sorted sub-arrays into one. See following C implementation for details.
 
@@ -25,14 +26,17 @@ Sorting In Place: No in a typical implementation
 Stable: Yes
 
 Applications of Merge Sort
-Merge Sort is useful for sorting linked lists in O(nLogn) time.In the case of linked lists, the case is different mainly
-due to the difference in memory allocation of arrays and linked lists. Unlike arrays, linked list nodes may not be adjacent in memory. 
-Unlike an array, in the linked list,
-we can insert items in the middle in O(1) extra space and O(1) time. Therefore merge operation of merge sort can be implemented without extra
-space for linked lists.In arrays, we can do random access as elements are contiguous in memory. Let us say we have an integer (4-byte) array A
-and let the address of A[0] be x then to access A[i], we can directly access the memory at (x + i*4). Unlike arrays, we can not do random
-access in the linked list. Quick Sort requires a lot of this kind of access. In linked list to access i’th index, we have to travel each and 
-every node from the head to i’th node as we don’t have a continuous block of memory. Therefore, the overhead increases for quicksort.
+Merge Sort is useful for sorting linked lists in O(nLogn) time.In the case of linked lists,
+the case is different mainly due to the difference in memory allocation of arrays and
+linked lists. Unlike arrays, linked list nodes may not be adjacent in memory. 
+Unlike an array, in the linked list,we can insert items in the middle in O(1) 
+extra space and O(1) time. Therefore merge operation of merge sort can be implemented 
+without extra space for linked lists.In arrays, we can do random access as elements are 
+contiguous in memory. Let us say we have an integer (4-byte) array A and let the address of A[0]
+be x then to access A[i], we can directly access the memory at (x + i*4). Unlike arrays, we can not do random access in the linked list. Quick Sort requires a lot of this kind of access.
+In linked list to access i’th index, we have to travel each and 
+every node from the head to i’th node as we don’t have a continuous block of memory.
+Therefore, the overhead increases for quicksort.
 Merge sort accesses 
 data sequentially and the need of random access is low.
 
